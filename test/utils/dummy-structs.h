d02f5ee35ef940e51b40f35b3365c2c05dcbfbea
Mateusz Przybyla <bearbytes.code@gmail.com>
2023-12-09 16:14:25 -0500

    Add more dummy structs for testing.

--- test/utils/dummy-structs.h
+++ test/utils/dummy-structs.h
@@ -3,6 +3,50 @@
 namespace bb::test
 {
 
+struct copy_only_t {
+    copy_only_t() = default;
+    ~copy_only_t() = default;
+
+    copy_only_t( copy_only_t const & ) = default;
+    copy_only_t( copy_only_t && ) = delete;
+
+    auto operator=( copy_only_t const & ) -> copy_only_t & = default;
+    auto operator=( copy_only_t && ) -> copy_only_t & = delete;
+};
+
+struct move_only_t {
+    move_only_t() = default;
+    ~move_only_t() = default;
+
+    move_only_t( move_only_t const & ) = delete;
+    move_only_t( move_only_t && ) = default;
+
+    auto operator=( move_only_t const & ) -> move_only_t & = delete;
+    auto operator=( move_only_t && ) -> move_only_t & = default;
+};
+
+struct copy_move_t {
+    copy_move_t() = default;
+    ~copy_move_t() = default;
+
+    copy_move_t( copy_move_t const & ) = default;
+    copy_move_t( copy_move_t && ) = default;
+
+    auto operator=( copy_move_t const & ) -> copy_move_t & = default;
+    auto operator=( copy_move_t && ) -> copy_move_t & = default;
+};
+
+struct no_copy_or_move_t {
+    no_copy_or_move_t() = default;
+    ~no_copy_or_move_t() = default;
+
+    no_copy_or_move_t( no_copy_or_move_t const & ) = delete;
+    no_copy_or_move_t( no_copy_or_move_t && ) = delete;
+
+    auto operator=( no_copy_or_move_t const & ) -> no_copy_or_move_t & = delete;
+    auto operator=( no_copy_or_move_t && ) -> no_copy_or_move_t & = delete;
+};
+
 struct S {
     constexpr S() = default;
     constexpr ~S() = default;
